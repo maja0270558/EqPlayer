61d1c135d2c0ff4dd0c5a559f4fd0a90012556d1
Jojo Lin <maja0270558@gmail.com>
2018-05-06 17:02:12 +0800

    Django Git init on this project

---
+++ EqPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EqPlayer
+//
+//  Created by 大容 林 on 2018/5/6.
+//  Copyright © 2018年 Django. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
